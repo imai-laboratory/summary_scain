b280bb2da3667c7e31e6cccd101633a1aaa199cd
tomoyukimaekawa <maekawa@ailab.ics.keio.ac.jp>
2023-08-25 16:10:48 +0900

    implement assistant

--- src/prompts.py
+++ src/prompts.py
@@ -20,3 +20,8 @@ def important_chat_ja(dialogue):
     joined_dialogue = "\n".join(numbered_dialogue)
     prompt = f"会話文から最も重要な発言を選びます．発言に番号をつけた一連の会話文が入力されます．会話文から最も重要な発言を2つ選び，その番号を書きます．\n\n# 出力の形式\n[発言の番号1, 発言の番号2]\n\n# 会話文\n{joined_dialogue}\n\n# 重要な発言\n"
     return prompt
+
+def assist_chat_ja(joined_dialogue):
+    system = "あなたは対話を補助するAIです．\nAとBの対話をCが聞いています．\n対話を補助するAIとして，対話の後に続けてCの発言を促してください．\n「AI: 」の後にあなたの発言を書いてください．"
+    user_ut = joined_dialogue
+    return system, user_ut
