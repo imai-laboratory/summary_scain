633ecba5f0e90b3fc1a8d6c96fe70547835d1d79
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2023-10-17 23:27:18 +0900

    first commit

--- src/extraction.py
+++ src/extraction.py
@@ -6,68 +6,166 @@ import tiktoken
 import logging
 import extractor
 
+from importlib.machinery import OPTIMIZED_BYTECODE_SUFFIXES
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt = '%m/%d/%Y %H:%M:%S',
                     level = logging.INFO)
 logger = logging.getLogger(__name__)
 
 class Params:
-    GPT_MODEL_COMPLETION = "gpt-3.5-turbo"
-    #GPT_MODEL_COMPLETION = "text-davinci-003"
+    #GPT_MODEL_COMPLETION = "gpt-3.5-turbo"
+    GPT_MODEL_COMPLETION = "text-davinci-003"
     GPT_MODEL_EMBEDDING = "text-embedding-ada-002"
     DIALOGUE_PATH = "./dat/"
     DIALOGUE_FILENAME = "PP"
+    FILENAME = "PP4.txt"
+    FILEPATH = DIALOGUE_PATH + FILENAME
     SPEAKERS = ["A: ", "B: "]
     DIALOGUE_START = 0
-    DIALOGUE_END = 100
+    DIALOGUE_END = 1
     MAX_TOKENS = 200
     TEMPERATURE = 0
     STOP_WORDS = "\n"
     USE_DUMMY = 0
-    RESULTS_PATH = "./results/generation/important_chat/"
+    RESULTS_PATH = "./results/generation/interpretation/"
+    #A_CONDITION = "Bの同級生だが，まだBの性格をあまり知らない人物"
+    #B_CONDITION = "普段成績が良く，謙虚な人間"
+    def setFileName(self, filename):
+        type(self).FILENAME = filename
+        type(self).FILEPATH = type(self).DIALOGUE_PATH + type(self).FILENAME
+        print(type(self).FILENAME)
+        print(type(self).FILEPATH)
 
-if __name__ == "__main__":
+def dialogue():
+    params = Params()
+    with open(params.FILEPATH) as f:
+        dialogue = [s.rstrip() for s in f.readlines()]
+    return dialogue
+
+def generate(A_CONDITION, B_CONDITION):
     params = Params()
 
     # Encoder
     encoding = tiktoken.encoding_for_model(params.GPT_MODEL_COMPLETION)
 
-    # Get list of dialogues
-    file_list = os.listdir(params.DIALOGUE_PATH)
-    file_list = [f for f in file_list if f.startswith(params.DIALOGUE_FILENAME)]
-    file_list = file_list[params.DIALOGUE_START:params.DIALOGUE_END]
-    #logger.debug(file_list)
+    # file_path = params.DIALOGUE_PATH + params.FILENAME
+    # with open(file_path) as f:
+    #     dialogue = [s.rstrip() for s in f.readlines()]
+    ex = extractor.InterpretationExtractor(params, encoding, dialogue(), params.FILEPATH)
+    #results = ex.interpretation(sum([1 for _ in open(file_path)]), params.A_CONDITION, params.B_CONDITION)
+    results = ex.interpretation(sum([1 for _ in open(params.FILEPATH)]), A_CONDITION, B_CONDITION)
 
-    # Loop
-    n_tokens = []
-    results = []
-    for filename in file_list:
-        logger.info("------ dialogue: {}".format(filename))
-        with open(params.DIALOGUE_PATH + filename) as f:
-            dialogue = [s.rstrip() for s in f.readlines()]
+    #time.sleep(3)
 
-        ex = extractor.SCAINExtractor(params, encoding, dialogue, filename)
-        #ex = extractor.ImportantExtractor(params, encoding, dialogue, filename)
-        n_token = ex.extract()
-        logger.info("------ number of tokens: {}".format(n_token))
-        n_tokens.append(n_token)
-        results.extend(ex.results)
-
-        # Wait to avoid overloading
-        time.sleep(3)
-        
-        # Save results for each dialogue
-        dt_now = datetime.datetime.now()
-        results_filename = params.RESULTS_PATH + dt_now.strftime("%Y%m%d_%H%M%S") + ".csv"
-        with open(results_filename, "w") as f:
-            writer = csv.writer(f)
-            writer.writerows(ex.results)
-
-    logger.info("------ number of total tokens: {}".format(sum(n_tokens)))
-
-    # Save results at once
     # dt_now = datetime.datetime.now()
     # results_filename = params.RESULTS_PATH + dt_now.strftime("%Y%m%d_%H%M%S") + ".csv"
     # with open(results_filename, "w") as f:
     #     writer = csv.writer(f)
     #     writer.writerows(results)
+    
+    return results
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    params = Params()
+    output=""
+    A_CONDITION = ""
+    B_CONDITION = ""
+    log = dialogue()
+    resultA=""
+    resultB=""
+    values=[]
+    for i in range(sum([1 for _ in open(params.FILEPATH)])):
+        values.append("")
+
+    if request.method == 'POST':
+        if request.form['action'] == 'select':
+            params.setFileName(request.form.get('conversation'))
+            log = dialogue()
+            values=[]
+            for i in range(sum([1 for _ in open(params.FILEPATH)])):
+                values.append("")
+            return render_template('index.html', dialogue=zip(log, values), filename=params.FILENAME, resultA=resultA, resultB=resultB)
+
+        elif request.form['action'] == 'generate':
+            A_CONDITION = request.form.get('A_Condition')  # フォームから目標を取得
+            B_CONDITION = request.form.get('B_Condition')
+
+            if A_CONDITION and B_CONDITION:
+                output = generate(A_CONDITION, B_CONDITION)
+                resultA = output[0]
+                resultB = output[1]
+                values = output[2]
+                return render_template('index.html', resultA=resultA, resultB=resultB, dialogue=zip(log, values), filename=params.FILENAME, A_CONDITION=A_CONDITION, B_CONDITION=B_CONDITION)
+            else:
+                return render_template('index.html', dialogue=zip(log, values), filename=params.FILENAME, resultA=resultA, resultB=resultB)
+        else:
+            return render_template('index.html', dialogue=zip(log, values), filename=params.FILENAME, resultA=resultA, resultB=resultB)
+
+    if request.method == "GET":
+        return render_template('index.html', dialogue=zip(log, values), filename=params.FILENAME, resultA=resultA, resultB=resultB)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# if __name__ == "__main__":
+#     params = Params()
+
+#     # Encoder
+#     encoding = tiktoken.encoding_for_model(params.GPT_MODEL_COMPLETION)
+
+#     # Get list of dialogues
+#     # file_list = os.listdir(params.DIALOGUE_PATH)
+#     # file_list = [f for f in file_list if f.startswith(params.DIALOGUE_FILENAME)]
+#     # file_list = file_list[params.DIALOGUE_START:params.DIALOGUE_END]
+#     # logger.debug(file_list)
+
+#     # Loop
+#     #n_tokens = []
+#     #results = []
+#     # for filename in file_list:
+#     #     logger.info("------ dialogue: {}".format(filename))
+#     #     with open(params.DIALOGUE_PATH + filename) as f:
+#     #         dialogue = [s.rstrip() for s in f.readlines()]
+
+#     #     ex = extractor.SCAINExtractor(params, encoding, dialogue, filename)
+#     #     #ex = extractor.ImportantExtractor(params, encoding, dialogue, filename)
+#     #     n_token = ex.extract()
+#     #     logger.info("------ number of tokens: {}".format(n_token))
+#     #     n_tokens.append(n_token)
+#     #     results.extend(ex.results)
+
+#     #     # Wait to avoid overloading
+#     #     time.sleep(3)
+        
+#     #     # Save results for each dialogue
+#     #     dt_now = datetime.datetime.now()
+#     #     results_filename = params.RESULTS_PATH + dt_now.strftime("%Y%m%d_%H%M%S") + ".csv"
+#     #     with open(results_filename, "w") as f:
+#     #         writer = csv.writer(f)
+#     #         writer.writerows(ex.results)
+
+#     # logger.info("------ number of total tokens: {}".format(sum(n_tokens)))
+#     file_path = params.DIALOGUE_PATH + params.FILENAME
+#     with open(file_path) as f:
+#         dialogue = [s.rstrip() for s in f.readlines()]
+#     ex = extractor.InterpretationExtractor(params, encoding, dialogue, file_path)
+#     results = ex.interpretation(sum([1 for _ in open(file_path)]), params.A_CONDITION, params.B_CONDITION)
+#     #results.extend(results)
+#     time.sleep(3)
+
+#     dt_now = datetime.datetime.now()
+#     results_filename = params.RESULTS_PATH + dt_now.strftime("%Y%m%d_%H%M%S") + ".csv"
+#     with open(results_filename, "w") as f:
+#         writer = csv.writer(f)
+#         writer.writerows(results)
+#     # Save results at once
+#     # dt_now = datetime.datetime.now()
+#     # results_filename = params.RESULTS_PATH + dt_now.strftime("%Y%m%d_%H%M%S") + ".csv"
+#     # with open(results_filename, "w") as f:
+#     #     writer = csv.writer(f)
+#     #     writer.writerows(results)
--- src/extractor.py
+++ src/extractor.py
@@ -2,17 +2,21 @@ import os
 import datetime
 import json
 import ast
+import re
+from time import sleep
+from unittest import result
 import openai
 import logging
 import prompts
 import utils
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = ""
 
-logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt = '%m/%d/%Y %H:%M:%S',
-                    level = logging.DEBUG)
-logger = logging.getLogger(__name__)
+# logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+#                     datefmt = '%m/%d/%Y %H:%M:%S',
+#                     level = logging.DEBUG)
+# logger = logging.getLogger(__name__)
 
 class Extractor():
     def __init__(self, params, encoding, dialogue, dialogue_id):
@@ -40,20 +44,20 @@ class Extractor():
                 prompt=prompt,
                 max_tokens=params.MAX_TOKENS,
                 temperature=params.TEMPERATURE,
-                stop=params.STOP_WORDS
+                #stop=params.STOP_WORDS
             )
         else:
-            logger.error("GPT model name is inappropriate")
+            #logger.error("GPT model name is inappropriate")
             exit()
 
         # Save log
-        dt_now = datetime.datetime.now()
-        log_filename = "./log/" + dt_now.strftime("%Y%m%d_%H%M%S") + ".txt"
-        dict_log = {"prompt": prompt}
-        dict_log.update(completion)
-        json_log = json.dumps(dict_log, indent=4, ensure_ascii=False)
-        with open(log_filename, "w") as f:
-            f.write(json_log)
+        # dt_now = datetime.datetime.now()
+        # log_filename = "./log/" + dt_now.strftime("%Y%m%d_%H%M%S") + ".txt"
+        # dict_log = {"prompt": prompt}
+        # dict_log.update(completion)
+        # json_log = json.dumps(dict_log, indent=4, ensure_ascii=False)
+        # with open(log_filename, "w") as f:
+        #     f.write(json_log)
 
         return completion
 
@@ -136,7 +140,7 @@ class SCAINExtractor(Extractor):
         
         return similarity
     
-class ImportantExtractoror(Extractor):
+class ImportantExtractor(Extractor):
     def evaluate(self):
         important_sentences = self.detect_important_sentences(self.dialogue)
         result = [self.dialogue_id] + important_sentences # [self.dialogue_id, important_sentence_id[1], important_sentence_id[2]]
@@ -157,3 +161,51 @@ class ImportantExtractoror(Extractor):
         important_sentences_str = response.get("choices")[0].get("message").get("content")
         important_sentences_list = list(ast.literal_eval(important_sentences_str))
         return important_sentences_list
+
+class InterpretationExtractor(Extractor):
+    def interpretation(self, n, A_condition, B_condition):
+        params = self.params
+        interpretationsA=[]
+        interpretationsB=[]
+        values=[]
+        for i in range(1, n+1):
+            if (i % 2 == 1):
+                promptA = prompts.interpretation_A_A(self.dialogue, i, A_condition, B_condition)
+                promptB = prompts.interpretation_A_B(self.dialogue, i, A_condition, B_condition)
+            else:
+                promptA = prompts.interpretation_B_A(self.dialogue, i, A_condition, B_condition)
+                promptB = prompts.interpretation_B_B(self.dialogue, i, A_condition, B_condition)
+
+            if params.USE_DUMMY == 1:
+                responseA = utils.dummy_completion(promptA, encoding=self.encoding)
+                responseB = utils.dummy_completion(promptB, encoding=self.encoding)
+            else:
+                responseA = self.openai_api_completion(promptA)
+                responseB = self.openai_api_completion(promptB)
+
+            interpretationA = responseA.get("choices")[0].get("text")
+            interpretationB = responseB.get("choices")[0].get("text")
+            similarity = self.calc_similarity(interpretationA, interpretationB)
+            #result = [self.dialogue_id, i, "Aの解釈", interpretationA, "Bの解釈", interpretationB, similarity]
+            interpretationsA.append(interpretationA)
+            interpretationsB.append(interpretationB)
+            values.append(round(similarity*100, 2))
+
+        return [interpretationsA, interpretationsB, values]
+    def calc_similarity(self, sentence1, sentence2):
+        params = self.params
+
+        if params.USE_DUMMY == 1:
+            similarity = 1
+        else:
+            embedding1 = openai.Embedding.create(
+                input=sentence1,
+                model=params.GPT_MODEL_EMBEDDING
+            )["data"][0]["embedding"]
+            embedding2 = openai.Embedding.create(
+                input=sentence2,
+                model=params.GPT_MODEL_EMBEDDING
+            )["data"][0]["embedding"]
+            similarity = utils.cosine_similarity(embedding1, embedding2)
+        
+        return similarity
